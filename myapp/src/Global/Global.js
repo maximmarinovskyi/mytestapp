e047bcbacfccd8aeca4f0f4d61d5710a1996390a
Max Marinovskyi <max0936003333@gmail.com>
2023-05-16 15:29:00 +0300

    First commit

---
+++ myapp/src/Global/Global.js
@@ -0,0 +1,11 @@
+import './Global.css';
+
+function Global() {
+  return (
+    <div className="Global">
+      1
+    </div >
+  );
+}
+
+export default Global;
---
+++ myapp/src/Main/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Main.css';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div className="Main" >
+        Main
+      </div >
+    );
+  }
+}
+export default Main;
